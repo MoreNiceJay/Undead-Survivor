d20ba37e070612b3465c7abbbec0c76e1b2bd193
MoreNiceJay <morenicejay@gmail.com>
2025-10-07 16:10:01 +0900

    monster object and ui created

---
+++ Assets/Monster.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+
+    public float speed;
+    public Rigidbody2D target;
+
+    bool isLive;
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 dirVec = (target.position - rigid.position).normalized;
+        Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+        rigid.linearVelocity = Vector2.zero;
+    }
+}
